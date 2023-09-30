ec5972e808196b6d66b2c3c00b62729ab56bfa76
Борис <59114008+Fox-sys@users.noreply.github.com>
2023-09-30 07:25:39 +0300

    Поправить задания по numpy

--- f1.py
+++ f1.py
@@ -1,20 +1,46 @@
-import numpy
+import math
+
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-import math
+import numpy
+import scipy
 
 mpl.use('TkAgg')
 fig, ax = plt.subplots()
 
-h = 0.5
-x_range = numpy.arange(-2, 3, h)
+
+def task_1():
+    """
+    Составить схему алгоритма и написать программу вычисления таблицы значений для заданной функции и построить график
+    """
+    h = 0.5
+    x_range = numpy.arange(-2, 3, h)
+    k_range = numpy.arange(1, 6)
+
+    def f(x, k):
+        return x * k
+    #
+    k_upper = numpy.sum(((-2) ** k_range) * scipy.special.factorial(k_range+1))
+    x_lower = x_range + 2.5
+    k_lower = k_range + 1
+    f_table = k_upper / numpy.array([x_lower ** i for i in k_lower]).sum(axis=0)
+
+    ax.plot(x_range, f_table)
+    plt.show()
 
 
-def f(x, k):
-    return (((-2) ** (k + 1)) * math.factorial(k + 1)) / ((x + 2.5) ** (k + 1))
+def task_2():
+    """
+    Составить схему алгоритма и написать программу вычисления таблицы значений для заданной функции и построить график
+    """
+    h = 0.25
+    x_range = numpy.arange(-5, 5, h)
+    k_range = numpy.arange(1, 6)
+    pi_range = numpy.pi ** (x_range * 3.5)
+    upper_range = numpy.sum(numpy.array([x_range ** i for i in k_range]), axis=0)
+    f_table = numpy.append(upper_range[x_range > 0], pi_range[x_range <= 0])
+    ax.plot(x_range, f_table)
+    plt.show()
 
 
-f_table = [sum([f(i, j) for j in range(1, 6)]) for i in x_range]
-
-ax.plot(x_range, f_table)
-plt.show()
+task_1()
--- f2.py
+++ f2.py
@@ -1,14 +1,42 @@
 import numpy as np
 
 
-data = np.array([])
-counter = 0
-while counter < 8:
-    try:
-        num = int(input('Введите число: '))
-    except ValueError:
-        print('Попытка не увенчалась успехом')
-        continue
-    data = np.append(data, num)
-    counter += 1
-print(np.array(list(map(lambda x: x % 10, data))))
+def task_1():
+    """
+    Ввести массив состоящий из 8 двух значных чисел целого типа.
+    Получить массив младших разрядов этих чисел.
+    :return:
+    """
+    data = np.array([])
+    counter = 0
+    while counter < 8:
+        try:
+            num = int(input('Введите число: '))
+        except ValueError:
+            print('Попытка не увенчалась успехом')
+            continue
+        data = np.append(data, num)
+        counter += 1
+    print(data % 10)
+
+
+def task_2():
+    """
+
+    :return:
+    """
+    data = np.array([])
+    counter = 0
+    while counter < 10:
+        try:
+            num = int(input('Введите число: '))
+        except ValueError:
+            print('Попытка не увенчалась успехом')
+            continue
+        data = np.append(data, num)
+        counter += 1
+    mask = 0 < data
+    print(np.sum(data[0:np.where(mask == False)[0][0]]))
+
+
+task_2()
